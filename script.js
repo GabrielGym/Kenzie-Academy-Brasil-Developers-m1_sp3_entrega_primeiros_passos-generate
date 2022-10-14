3530554557ed40fe742aeea3684263fc8dc1e9c0
Gabriel_ggm <biel.ferreira12342@gmail.com>
2022-10-13 23:02:28 -0300

    Primeiros passos

---
+++ script.js
@@ -0,0 +1,98 @@
+// Exercicio 1 completo //
+
+
+// let numeroInteiro1 = parseInt(prompt("Digite um numero inteiro"));
+// let numeroInteiro2 = parseInt(prompt("Digite outro numero inteiro"));
+// let diferencaEntreOsNumeros = numeroInteiro1 - numeroInteiro2;
+
+// if (numeroInteiro1 > numeroInteiro2) {
+//     alert(`O maior número é ${numeroInteiro1} e a diferença é ${diferencaEntreOsNumeros}`)
+// } else {
+//     alert(`O maior número é ${numeroInteiro2} e a diferença é ${diferencaEntreOsNumeros}`)
+// };
+
+
+// Exercicio 2 completo //
+
+
+// let numero1 = parseFloat(prompt("Digite um numero"));
+// let numero2 = parseFloat(prompt("Digite outro numero"));
+
+// if (numero1 > numero2) {
+//     alert(`O número ${numero1} é maior que número ${numero2}`)
+// } else if (numero1 == numero2) {
+//     alert(`Números iguais`)
+// } else {
+//     alert(`O número ${numero2} é maior que número ${numero1}`)
+// };
+
+
+// Exercicio 3 incompleto nao aparece o valor correto //
+
+// let salarioTrabalhador = parseFloat(prompt("Digite o salario do trabalhador"));
+// let prestacaoEmprestimo = parseFloat(prompt("Digite o valor da prestação do empréstimo"));
+
+// if (prestacaoEmprestimo == 30% salarioTrabalhador) {
+//     alert(`Empréstimo não concedido`)
+// } else {
+//     alert(`Empréstimo concedido`)
+// };
+
+
+// Exercicio 4 completo //
+
+// let escolhaDeNumero = parseFloat(prompt("Digite um numero"));
+
+// if (escolhaDeNumero %3 == 0 && escolhaDeNumero %5 == 0){
+//     alert(`Valor inválido`)
+// } else if (escolhaDeNumero %3 == 0){
+//     alert(`Valor divisível por 3`)
+// } else if (escolhaDeNumero %5 == 0){
+//     alert(`Valor divisível por 5`)
+// } else if (escolhaDeNumero %2 == 0){
+//     alert(`É par`)
+// } else {
+//     alert(`É impar`)
+// };
+
+
+// Exercicio 5 completo //
+
+
+// let diaDaSemana = parseInt(prompt("Digite um numero de 1 a 7"));
+
+// if (diaDaSemana === 1) {
+//     alert(`Domingo`)
+// } else if (diaDaSemana === 2) {
+//     alert(`Segunda-Feira`)
+// } else if (diaDaSemana === 3) {
+//     alert(`Terça-Feira`)
+// } else if (diaDaSemana === 4) {
+//     alert(`Quarta-Feira`)
+// } else if (diaDaSemana === 5) {
+//     alert(`Quinta-Feira`)
+// } else if (diaDaSemana === 6) {
+//     alert(`Sexta-Feira`)
+// } else if (diaDaSemana === 7) {
+//     alert(`Sábado`)
+// } else {
+//     alert(`Numero inválido`)
+// };
+
+
+// Exercicio 6 completo //
+
+
+// let salarioFuncionario = parseFloat(prompt("Digite o salário do funcionário"));
+// let reajusteSalario = parseFloat(prompt("Digite o reajuste do salário do funcionário"));
+
+// if (reajusteSalario <= 5){
+//     alert(`O salário do funcionário corrigido é de: ${salarioFuncionario + (salarioFuncionario * reajusteSalario) / 100}`)
+// } else {
+//     alert(`O reajuste acima de 5% é inválido`)
+// };
+
+
+// Exercicio 7 //
+
+
